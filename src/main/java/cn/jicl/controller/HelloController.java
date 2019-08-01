33798ad37050fe455e10a29d4637911b6fdaf03b
xianzilei <1739575190@qq.com>
2019-08-01 23:03:37 +0800

    RequestMapping注解学习一

--- src/main/java/cn/jicl/controller/HelloController.java
+++ src/main/java/cn/jicl/controller/HelloController.java
@@ -2,6 +2,7 @@ package cn.jicl.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @Auther: xianzilei
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
+    //其中的/可以省略不写
+    @RequestMapping(value = "/hello",method = RequestMethod.POST)
     public String hello(){
         System.out.println("处理请求完成！");
         //视图解析器字段拼串
